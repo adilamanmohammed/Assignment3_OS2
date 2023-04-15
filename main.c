9ea7b8c448d8153d38c298879ad9a2f7cda3095c
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-04-15 14:05:32 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include "library.h"
 
+int a,b,c,d;
+
+int main()
+{
+
+printf("enter option:\n");
+scanf("%d",&d);
+printf("enter funtion 1 0r 2:\n");
+scanf("%d %d",&a,&b);
+
+if(d==1)
+sum(a,b);
+else
+sub(a,b);
+
+return 0;
+}
