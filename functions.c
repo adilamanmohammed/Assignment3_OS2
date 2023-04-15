d1184d51c02b751913cd6b0f8b3a4dac8ad740a3
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-04-15 14:12:09 -0500

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
 
+int sum(int a,int b)
+{
+    int temp;
+    printf("%d",temp=a+b);
+    return 0;
+}
+
+int sub(int a, int b)
+{
+    int temp;
+    printf("%d",temp=a-b);
+    return 0;
+}
