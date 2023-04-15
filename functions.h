2ae3354482c98871ad5de33064f236240ab4d402
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-04-15 14:39:55 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,3 +1,8 @@
-int sum(int a,int b);
-
-int sub(int a, int b);
+int search_Item(char *);
+int search_Item_LRU(char *);
+void print_List();
+void insert_Operation(char *);
+void delete_Node();
+void delete_Node_LRU(int);
+void FIFO_algorithm(int, int);
+void LRU_algorithm(int, int);
